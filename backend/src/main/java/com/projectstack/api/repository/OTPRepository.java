3c4ef5e2903ec2decd24d8319e82b418bd0b6e64
Santhosh <santikumr065@gmail.com>
2025-03-22 16:46:32 +0530

    Delete backend directory

--- backend/src/main/java/com/projectstack/api/repository/OTPRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectstack.api.repository;
-
-import com.projectstack.api.model.OTP;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface OTPRepository extends MongoRepository<OTP, String> {
-    Optional<OTP> findByEmail(String email);
-}
--- backend/src/main/java/com/projectstack/api/repository/TokenBlacklistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projectstack.api.repository;
-
-import com.projectstack.api.model.TokenBlacklist;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TokenBlacklistRepository extends MongoRepository<TokenBlacklist, String> {
-    boolean existsByToken(String token);
-}
